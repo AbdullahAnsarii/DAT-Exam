83a41f3291d9e147d5c1c5a03f7dcea2a3f2285b
Abdullah Ansari <aa9272137@gmail.com>
2022-08-19 12:14:54 +0500

    Hero done
    Task 1

---
+++ components/Hero.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import Image from 'next/image';
+import styles from "../styles/Hero.module.scss";
+
+const Hero: FC = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.detail}>
+                <div >
+                    <h1 className={styles.heading}> Different Spice For A Different Taste </h1>
+                </div>
+                <div>
+                    <p className={styles.paragraph}>
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit,
+                        nulla enim posuere quis consequat.
+                    </p>
+                </div>
+
+                <div>
+                    <button className={styles.button}>Get Started</button>
+                </div>
+            </div>
+            <div className={styles.image}>
+                <Image src='/Slider Food Image.png' alt="" width="800" height="500" />
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.scss'
 
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Navbar />
+      <Hero />
 
     </div>
   )
